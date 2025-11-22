f522ff0f902503ad0d6186e098e00b6958369e2b
ander <richardanderdev009@gmail.com>
2025-11-22 08:18:29 -0400

    Metodos para agregar, extraer detalles de una orden y obtener una orden con sus detalles

--- Repositories/OrderRepository.go
+++ Repositories/OrderRepository.go
@@ -1,6 +1,8 @@
 package Repositories
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 	"orderflow.com/v2/models"
 )
@@ -18,4 +20,44 @@ func NewOrderRepository(ctx *gorm.DB) *OrderRepository {
 			},
 		},
 	}
+}
+
+// AddDetail function to add detail to order
+func (order *OrderRepository) AddDetail(orderDetail models.OrderDetail) error {
+	err := order.ctx.Save(&orderDetail).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// RemoveDetail function to remove details
+func (order *OrderRepository) RemoveDetail(idDetail uint) error {
+	var orderDetail models.OrderDetail
+	err := order.ctx.Find(&orderDetail, idDetail).Error
+	if err != nil {
+		return err
+	}
+	if orderDetail.ID == 0 {
+		return errors.New("does not exist")
+	}
+	errDel := order.ctx.Delete(&orderDetail).Error
+	if errDel != nil {
+		return err
+	}
+	return nil
+}
+
+// GetOrderWithDetails function
+func (order *OrderRepository) GetOrderWithDetails(orderId uint) (models.Order, error) {
+	var orderDetail models.Order
+	//get order with relations
+	err := order.ctx.
+		Preload("OrderDetail").
+		Find(&orderDetail, orderId).Error
+
+	if err != nil {
+		return models.Order{}, err
+	}
+	return orderDetail, nil
 }
--- contracts/repositories/IOrderRepository.go
+++ contracts/repositories/IOrderRepository.go
@@ -4,4 +4,7 @@ import "orderflow.com/v2/models"
 
 type IOrderRepository interface {
 	IBaseRepositoryWithClientId[models.Order]
+	AddDetail(orderDetail models.OrderDetail)
+	RemoveDetail(idDetail uint) error
+	GetOrderWithDetails(orderId uint) (models.Order, error)
 }
